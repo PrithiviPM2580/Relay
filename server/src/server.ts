3d0d69b11085fe4607368586ed705ee6979cc4f4
Prithivi <prithivi@users.noreply.github.com>
2026-01-22 23:14:35 +0545

    feat: refactor server setup to initialize HTTP server and integrate socket server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,12 @@
-import app from "./app.js";
+import http from "node:http";
 import config from "@/config/env.config.js";
 import logger from "@/lib/logger.lib.js";
+import initSockerServer from "./sockets/index.js";
+import app from "./app.js";
+
+const httpServer = http.createServer(app);
+
+initSockerServer(httpServer);
 
 const PORT = config.PORT || 3000;
 
