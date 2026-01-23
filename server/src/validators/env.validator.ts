460353cd4250e57da5938a6d112f86f641bc878c
Prithivi <prithivi@users.noreply.github.com>
2026-01-23 16:26:35 +0545

    feat: initialize client with React and Vite setup

--- server/src/validators/env.validator.ts
+++ server/src/validators/env.validator.ts
@@ -6,7 +6,7 @@ export const envValidator = z.object({
     .default("development"),
   PORT: z.coerce.number().default(5000),
   LOG_LEVEL: z.string().default("info"),
-  MONGO_URI: z.string().min(1, "MONGO_URI is required"),
+  DATABASE_URL: z.string().min(1, "MONGO_URI is required"),
   CORS_ORIGIN: z.string().default("http://localhost:5173"),
   BETTER_AUTH_SECRET: z.string().min(1, "BETTER_AUTH_SECRET is required"),
   BETTER_AUTH_URL: z.string().min(1, "BETTER_AUTH_URL is required"),
